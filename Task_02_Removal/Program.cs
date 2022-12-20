98a2bd8fbcc8834ba85d73a4696b72c40626dc44
Zhanar S <zhan-ct@yandex.ru>
2022-12-20 19:06:43 +0300

    Добавлено решение по задаче 10

---
+++ Task_02_Removal/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №10. Написать программу, которая выводит случайное трехзначное число и удаляет вторую цифру из этого числа
+
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+
+int number1 = number % 1000/100; // формула для поиска первой цифры из остатка (1000 / 100 = 10, т.е. первая цифра остатка)
+Console.WriteLine(number1);
+
+int number3 = number%10;
+Console.WriteLine(number3);
+
+Console.Write(number1);
+Console.WriteLine(number3);
+
+/* второй способ решения - вместо команд, начиная с 6-строки "int number1 = number % 1000/100;"
+
+string newNumber = "";
+newNumber += number % 1000/100;
+newNumber += number % 10;
+Console.WriteLine(newNumber);
+*/
