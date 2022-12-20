bab198e0383a037b82a1036e9268b1468a48c8f2
Zhanar S <zhan-ct@yandex.ru>
2022-12-20 19:06:11 +0300

    Добавлено решение по задаче семинара

---
+++ Task_01_Comparison/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти наибольшую цифра случайного числа из отрезка [10,99]
+
+int number = new Random().Next(10,100);
+Console.WriteLine(number);
+
+if(number/10 > number%10)
+{
+    Console.WriteLine(number/10);
+}
+else
+{
+    Console.WriteLine(number%10);
+}
